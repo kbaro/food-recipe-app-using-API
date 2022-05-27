0af315b2596fd5cfe211b617260c42c866f6eedc
Kristijan Baro <baro.kristijan@gmail.com>
2022-05-28 00:13:45 +0200

    s

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function getMealList(){
 // get recipe of the meal
 function getMealRecipe(e){
     e.preventDefault();
-    if(e.target.classList.contain('recipe-btn')){
+    if(e.target.classList.contains('recipe-btn')){
         let mealItem = e.target.parentElement.parentElement;
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
         .then(response => response.json())
